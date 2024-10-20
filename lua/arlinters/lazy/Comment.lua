fd82afc2a98240c866bf78efda239241702ff3bc
Charles Winters <charlie.winters@gmail.com>
2024-10-20 10:09:15 -0700

    Add Comment.nvim

---
+++ lua/arlinters/lazy/Comment.lua
@@ -0,0 +1,14 @@
+return {
+	"numToStr/Comment.nvim",
+	config = {
+		toggler = {
+			line = "<leader>cc",
+			block = "leader>cb",
+		},
+		extra = {
+			above = "<leader>cO",
+			below = "<leader>co",
+			eol = "<leader>cA",
+		},
+	},
+}
