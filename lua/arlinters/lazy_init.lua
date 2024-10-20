01328e0f129109311238012461399050ddf04350
Charles Winters <charlie.winters@gmail.com>
2024-10-20 09:20:23 -0700

    add harpoon

--- lua/arlinters/lazy_init.lua
+++ lua/arlinters/lazy_init.lua
@@ -1,4 +1,3 @@
-print("hello from lazy")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
--- lua/arlinters/remap.lua
+++ lua/arlinters/remap.lua
@@ -1,3 +1,2 @@
-print("hello this is from the config")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
